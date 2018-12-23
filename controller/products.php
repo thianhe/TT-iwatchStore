ff2e455ee9455345095f32f7fb7a710df175f5f6
ngSgn <t105590049@ntut.org.tw>
2018-12-23 23:53:51 +0800

    product image carousel

--- controller/products.php
+++ controller/products.php
@@ -1,6 +1,6 @@
 <?php
 $_PageBeforeLoginOut = 'products';
-$productList =  Database::get()->execute('select * from WATCH w,company,operating_system o where company_id = brand_id and w.op_id = o.op_id;');
+$productList =  Database::get()->execute('select * from WATCH w,company,operating_system o where company_id = brand_id and w.op_id = o.op_id order by watch_id;');
 $brandList = Database::get()->execute('select * from company');
 $opList = Database::get()->execute('select * from operating_system');
 $opList[] = array_shift($opList);
--- view/body/product_info.php
+++ view/body/product_info.php
@@ -3,40 +3,60 @@
 </script>
 <div class="jumbotron" id="product_info">
     <div class="container">
-        <div class="container">
-            <div class="row" id="go_back">
-                <a href="products" class="btn">
-                    <i class="fas fa-arrow-left"></i>
-                    All Product
-                </a>
-            </div>
+        <div class="row" id="go_back">
+            <a href="products" class="btn">
+                <i class="fas fa-arrow-left"></i>
+                All Product
+            </a>
         </div>
         <div class="row">
             <div class="col-lg-7 col-md-12 col-sm-12">
-
-                    <div class="flexslider" style="width:350px; margin-left:100px;">
-                        <ul class="slides">
-                            <?php
-                            foreach($images as $image){
-                                echo'<li><img src='.$dir.$image.' alt="" width="350" height="250"></li>';
-                            }
-                            ?>
-                        </ul>
-                    </div>
-                    
-
-                    <div id="review_jumbotron" class="jumbotron">
-
-                        <h3>Review</h3>
-                        <hr>
-                        <span class="title_font">TONY<br></span>
-                        <span class="content_font">
-                            FUCKING TRASH
-                        </span>
-                        <hr>
-
-                    </div>
+            <div id="carouselExampleIndicators" class="carousel slide " data-ride="carousel">
+            <ol class="carousel-indicators">
+                <?php 
+                $i = 0;
+                for( $a=2; $a < count($files); $a++):
+                ?>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="<?php echo $i; ?>" class="<?php echo $i == 0 ? 'active': ''; ?>"></li>
+                <?php 
+                $i++;
+                endfor;
+                ?>
+            </ol>
+            <div id="home_watch_carousel"class="carousel-inner">
+                <?php 
+                $i = 0;
+                for( $a=2; $a < count($files); $a++):
+                ?>
+                <div class="carousel-item text-center <?php echo $i == 0 ? 'active':'' ;?>">
+                    <img src="<?php echo $dir.$files[$a];?>" alt="">
+                </div>
+                <?php 
+                $i++;
+                endfor;
+                ?>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+            <div id="review_jumbotron" class="jumbotron">
+
+                <h3>Review</h3>
+                <hr>
+                <span class="title_font">TONY<br></span>
+                <span class="content_font">
+                    FUCKING TRASH
+                </span>
+                <hr>
+
+            </div>
+        </div>
             <div class="col-lg-5 col-md-12 col-sm-12">
                 <div id="product_jumbotron" class="jumbotron">
                     <h3 class="jumbotron_title">
@@ -54,7 +74,7 @@
                     </span>
                     <hr>
                     <span class="title_font">Price<br></span>
-                    <span class="content_font">
+                    <span class="content_font">NT$
                         <?php echo $watchInfo['price'];?>
                     </span>
                     <hr>
